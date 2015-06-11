cd15a5da5641c29473319a6118dcbdeb5523e719
Michael Krieg <info@red-tag.de>
2015-06-11 10:12:37 +0200

    force version 4 to be installed. Otherwise, EPEL repos would have precedence.

--- manifests/repo/el6.pp
+++ manifests/repo/el6.pp
@@ -4,6 +4,7 @@ class varnish::repo::el6 {
     baseurl  => "https://repo.varnish-cache.org/redhat/varnish-${::varnish::varnish_version}/el6/",
     descr    => 'Varnish-cache RPM repository',
     enabled  => 1,
-    gpgcheck => 0
+    gpgcheck => 0,
+    priority => 1,
   }
 }
